05fb09f5b427c4695db19c8b0092668bea015fb2
matt2mi <2m1tema@gmail.com>
2019-02-13 18:07:41 +0100

    :basketball: add bootstrap + fake party with 3 shoots + start/cancel/restart btns actions

--- src/App.js
+++ src/App.js
@@ -5,53 +5,130 @@ import {Client} from "nes";
 
 class App extends Component {
 
+    countdownTimer;
+    cli = new Client('ws://localhost:3005');
+
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            connected: true,
+            countdown: 10,
+            score: 0,
+            partyStarted: false
         };
 
-        this.handleData = this.handleData.bind(this);
+        this.cancelParty = this.cancelParty.bind(this);
+        this.handleScore = this.handleScore.bind(this);
+        this.restart = this.restart.bind(this);
         this.start = this.start.bind(this);
+        this.startParty = this.startParty.bind(this);
 
+        // this.cli
+        //     .connect()
+        //     .then(() => {
+        //         console.log('connected');
+        //         this.setState({connected: true});
+        //     });
+    }
+
+    cancelParty() {
+        clearInterval(this.countdownTimer);
+        this.setState({
+            countdown: 10,
+            score: 0,
+            partyStarted: false
+        });
+        this.cli
+            .unsubscribe('/clutch')
+            .then((data) => {
+                if(data) {
+                    console.log(data.payload.msg);
+                }
+            });
+    }
+
+    handleScore(update, flags) {
+        if (this.state.countdown <= 29 && this.state.countdown > 0) {
+            this.setState({score: this.state.score + update.newScore});
+        }
+    }
+
+    restart() {
+        this.setState({
+            countdown: 10,
+            score: 0,
+            partyStarted: false
+        });
         this.start();
     }
 
     start() {
-        const cli = new Client('ws://localhost:3005');
-
-        cli.connect()
-            .then(() => cli.subscribe('/clutch', this.handleData))
-            .then(() => cli.request('connection'))
-            .then((data) => console.log(data.payload.msg));
-    };
-
-    handleData(update, flags) {
-        console.log('update', update);
-        this.setState({count: this.state.count + update.counter});
+        this.cli
+            .subscribe('/clutch', this.handleScore)
+            .then(() => this.cli.request('start'))
+            .then((data) => {
+                console.log(data.payload.msg);
+                this.startParty();
+            });
     }
+
+    startParty() {
+        this.setState({partyStarted: true});
+        this.countdownTimer = setInterval(() => {
+            this.setState({countdown: this.state.countdown - 1});
+
+            if (this.state.countdown <= 0) {
+                clearInterval(this.countdownTimer);
+            }
+        }, 1000);
+    };
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+            <div className="container-fluid app">
+                {
+                    !this.state.connected ?
+                        <div className="row">
+                            <div className="col-12 text-center">
+                                <img src={logo} className="App-logo" alt="logo"/>
+                                <p>Waiting connection...</p>
+                            </div>
+                        </div> :
+                        <div>
+                            <div className="row">
+                                <div className="col-4">
+                                    {
+                                        !this.state.partyStarted ?
+                                            <button type="btn" className="btn btn-success" onClick={this.start}>
+                                                Start !
+                                            </button> :
+                                            this.state.countdown > 0 ?
+                                                <button type="btn" className="btn btn-danger"
+                                                        onClick={this.cancelParty}>
+                                                    Cancel
+                                                </button> :
+                                                <button type="btn" className="btn btn-primary" onClick={this.restart}>
+                                                    Restart !
+                                                </button>
 
-                <div>
-                    Count : {this.state.count}
-                </div>
+                                    }
+                                </div>
+                                <div className="col-4">
+                                    Score : {this.state.score}
+                                </div>
+                                <div className="col-4">
+                                    Temps restant : {this.state.partyStarted ? this.state.countdown : '-'}
+                                </div>
+                            </div>
+                            {
+                                this.state.countdown <= 0 &&
+                                <div className="row">
+                                    <div className="col-12 text-center">Bravo, t'as fait {this.state.score} points !!
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                }
             </div>
         );
     }
--- ws-server.js
+++ ws-server.js
@@ -2,24 +2,54 @@ const Hapi = require('hapi');
 const Nes = require('nes');
 
 const port = 3005;
-
 const server = new Hapi.Server({port});
+
+let countdownTimer;
+let score = 0;
+
+const stopParty = () => {
+    console.log('stop');
+    clearInterval(countdownTimer);
+    score = 0;
+};
+
+const startSimulatingScore = () => {
+    countdownTimer = setInterval(() => {
+        score = score + 2;
+        server.publish('/clutch', {newScore: 2});
+
+        if (score > 6) {
+            stopParty();
+        }
+    }, 3000);
+};
 
 const start = async () => {
     await server.register(Nes);
 
     server.route({
         method: 'GET',
-        path: '/connection',
+        path: '/start',
         config: {
-            id: 'connection',
+            id: 'start',
             handler: (request, h) => {
-                server.publish('/clutch', { counter: 3 });
-                server.publish('/clutch', { counter: 3 });
-                return {msg: 'connected !'};
+                startSimulatingScore();
+                return {msg: 'let\'s go !'};
             }
         }
     });
+    server.route({
+        method: 'GET',
+        path: '/stop',
+        config: {
+            id: 'stop',
+            handler: (request, h) => {
+                stopParty();
+                return {msg: 'party stopped'};
+            }
+        }
+    });
+
     server.subscription('/clutch');
     await server.start();
 
