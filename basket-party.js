1fb4a2dc3fbf288e23126e5fb6a40503291805d1
matt2mi <2m1tema@gmail.com>
2019-02-19 00:25:35 +0100

    :basketball: route to start party - step blinking on start

---
+++ basket-party.js
@@ -0,0 +1,138 @@
+const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+
+const LED1 = new Gpio(0, 'out');
+const LED2 = new Gpio(1, 'out');
+const LED3 = new Gpio(2, 'out');
+const LED4 = new Gpio(3, 'out');
+const LED5 = new Gpio(4, 'out');
+const LED6 = new Gpio(5, 'out');
+
+const blinkInterval = setInterval(blinkLedOnStart, 150); //run the blinkLED function every 250ms
+
+function blinkLedOnStart() { //function to start blinking
+    LED1.writeSync(!LED1.readSync());
+    LED2.writeSync(!LED1.readSync());
+    LED3.writeSync(!LED1.readSync());
+    LED4.writeSync(!LED1.readSync());
+    LED5.writeSync(!LED1.readSync());
+    LED6.writeSync(!LED1.readSync());
+}
+
+function endBlink() {
+    clearInterval(blinkInterval);
+    LED1.writeSync(0);
+    LED2.writeSync(0);
+    LED3.writeSync(0);
+    LED4.writeSync(0);
+    LED5.writeSync(0);
+    LED6.writeSync(0);
+}
+
+// LED.unexport(); // Unexport GPIO to free resources
+
+setTimeout(endBlink, 2000); //stop blinking after 5 seconds
+
+/*
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+print "+-----------------------------------------------------------+"
+print "|   Mesure de distance par le capteur ultrasonore HC-SR04   |"
+print "+-----------------------------------------------------------+"
+
+Trig = 23          # Entree Trig du HC-SR04 branchee au GPIO 23
+Echo = 24         # Sortie Echo du HC-SR04 branchee au GPIO 24
+
+GPIO.setup(Trig,GPIO.OUT)
+GPIO.setup(Echo,GPIO.IN)
+
+GPIO.setup(0,GPIO.OUT)
+GPIO.setup(1,GPIO.OUT)
+GPIO.setup(2,GPIO.OUT)
+GPIO.setup(3,GPIO.OUT)
+GPIO.setup(4,GPIO.OUT)
+GPIO.setup(5,GPIO.OUT)
+
+GPIO.output(0, True)
+GPIO.output(1, True)
+GPIO.output(2, True)
+GPIO.output(3, True)
+GPIO.output(4, True)
+GPIO.output(5, True)
+
+time.sleep(0.1)
+
+GPIO.output(0, False)
+GPIO.output(1, False)
+GPIO.output(2, False)
+GPIO.output(3, False)
+GPIO.output(4, False)
+GPIO.output(5, False)
+
+time.sleep(0.1)
+
+GPIO.output(Trig, False)
+
+# repet = input("Cmb de tps (ms) ? : ")
+
+score = 0
+swishing = False
+
+# for x in range(repet):    # On prend la mesure "repet" fois
+while True:
+time.sleep(0.001)       # On la prend toute les 1 milli-seconde
+
+GPIO.output(Trig, True)
+time.sleep(0.00001)
+GPIO.output(Trig, False)
+
+while GPIO.input(Echo)==0:  ## Emission de l'ultrason
+debutImpulsion = time.time()
+
+while GPIO.input(Echo)==1:   ## Retour de l'Echo
+finImpulsion = time.time()
+
+distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)  ## Vitesse du son = 340 m/s
+
+if swishing == False and distance <= 10:
+print "swish !!"
+swishing = True
+score = score + 2
+print "score: ",score," points"
+GPIO.output(0, True)
+time.sleep(0.05)
+GPIO.output(1, True)
+time.sleep(0.05)
+GPIO.output(2, True)
+time.sleep(0.05)
+GPIO.output(3, True)
+time.sleep(0.05)
+GPIO.output(4, True)
+time.sleep(0.05)
+GPIO.output(5, True)
+time.sleep(0.05)
+GPIO.output(0, False)
+time.sleep(0.05)
+GPIO.output(1, False)
+time.sleep(0.05)
+GPIO.output(2, False)
+time.sleep(0.05)
+GPIO.output(3, False)
+time.sleep(0.05)
+GPIO.output(4, False)
+time.sleep(0.05)
+GPIO.output(5, False)
+
+if swishing == True and distance > 10:
+print "fin de swish"
+swishing = False
+
+if score > 10:
+break
+
+GPIO.cleanup()
+*/
--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const Hapi = require('hapi');
 const Path = require('path');
-const Gpio = require('onoff').Gpio;
 
 // Create a server with a host and port
 const server = Hapi.server({
@@ -14,49 +13,6 @@ const server = Hapi.server({
         }
     }
 });
-
-// Flowing leds script
-//Put all the LED variables in an array
-//use declare variables for all the GPIO output pins
-const leds = [
-    new Gpio(4, 'out'),
-    new Gpio(17, 'out'),
-    new Gpio(27, 'out'),
-    new Gpio(22, 'out'),
-    new Gpio(18, 'out'),
-    new Gpio(23, 'out'),
-    new Gpio(24, 'out'),
-    new Gpio(25, 'out')
-];
-
-let indexCount = 0;
-//variable for flowing direction
-let dir = "up";
-
-const flowingLeds = () => {
-    leds.forEach(function (currentValue) {
-        currentValue.writeSync(0); //turn off LED
-    });
-    if (indexCount === 0) dir = "up"; //set flow direction to "up" if the count reaches zero
-    if (indexCount >= leds.length) dir = "down"; //set flow direction to "down" if the count reaches 7
-    if (dir === "down") indexCount--; //count downwards if direction is down
-    leds[indexCount].writeSync(1); //turn on LED that where array index matches count
-    if (dir === "up") indexCount++ //count upwards if direction is up
-};
-
-//run the flowingLeds function every 100ms
-let flowInterval;
-const startFlowing = () => {
-    flowInterval = setInterval(flowingLeds, 100);
-};
-const stopFlowing = () => {
-    //function to run when user closes using ctrl+cc
-    clearInterval(flowInterval); //stop flow interwal
-    leds.forEach(function (currentValue) { //for each LED
-        currentValue.writeSync(0); //turn off LED
-        currentValue.unexport(); //unexport GPIO
-    });
-};
 
 // Start the server
 const init = async () => {
@@ -84,24 +40,13 @@ const init = async () => {
     });
     server.route({
         method: 'GET',
-        path: '/start-flowing',
+        path: '/start',
         handler: (request, h) => {
             console.log('startFlowing');
-            startFlowing();
-            return 'Flowing started !';
+            require('basket-party');
+            return 'Party started !';
         }
     });
-    server.route({
-        method: 'GET',
-        path: '/stop-flowing',
-        handler: (request, h) => {
-            console.log('stopFlowing');
-            stopFlowing();
-            return 'Flowing stopped !';
-        }
-    });
-
-    console.log('flowing leds routes loaded');
 
     require('./ws-server');
 
