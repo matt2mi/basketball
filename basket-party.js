bf2bb62bddeb33b176009945b45c6e031b7b9a65
matt2mi <2m1tema@gmail.com>
2019-02-19 01:19:49 +0100

    :basketball: test first party #6

--- basket-party.js
+++ basket-party.js
@@ -7,10 +7,38 @@ const LED4 = new Gpio(3, 'out');
 const LED5 = new Gpio(4, 'out');
 const LED6 = new Gpio(5, 'out');
 
-const TRIG = new Gpio(23, 'out');
-const ECHO = new Gpio(24, 'in');
+// const TRIG = new Gpio(23, 'out');
+// const ECHO = new Gpio(24, 'in');
 
-(async () => {
+//Put all the LED variables in an array
+const leds = [LED1, LED2, LED3, LED4, LED5, LED6];
+let indexCount = 0; //a counter
+let dir = "up"; //variable for flowing direction
+
+const flowInterval = setInterval(flowingLeds, 100); //run the flowingLeds function every 100ms
+
+function flowingLeds() { //function for flowing Leds
+    leds.forEach(function(currentValue) { //for each item in array
+        currentValue.writeSync(0); //turn off LED
+    });
+    if (indexCount === 0) dir = "up"; //set flow direction to "up" if the count reaches zero
+    if (indexCount >= leds.length) dir = "down"; //set flow direction to "down" if the count reaches 7
+    if (dir === "down") indexCount--; //count downwards if direction is down
+    leds[indexCount].writeSync(1); //turn on LED that where array index matches count
+    if (dir === "up") indexCount++ //count upwards if direction is up
+}
+
+function unexportOnClose() { //function to run when exiting program
+    clearInterval(flowInterval); //stop flow interwal
+    leds.forEach(function(currentValue) { //for each LED
+        currentValue.writeSync(0); //turn off LED
+        currentValue.unexport(); //unexport GPIO
+    });
+}
+
+process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+cc
+
+/*(async () => {
     TRIG.writeSync(0);
 
     LED1.writeSync(0);
@@ -163,69 +191,7 @@ const ECHO = new Gpio(24, 'in');
     const startParty = Date.now();
     await gameOn();
     console.log(`Fin de partie en ${(Date.now() - startParty) / 1000} secondes`);
-})();
 
+    // GPIO.cleanup()
+})();*/
 
-/*
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-
-
-
-
-# for x in range(repet):    # On prend la mesure "repet" fois
-while True:
-time.sleep(0.001)       # On la prend toute les 1 milli-seconde
-
-GPIO.output(Trig, True)
-time.sleep(0.00001)
-GPIO.output(Trig, False)
-
-while GPIO.input(Echo)==0:  ## Emission de l'ultrason
-debutImpulsion = time.time()
-
-while GPIO.input(Echo)==1:   ## Retour de l'Echo
-finImpulsion = time.time()
-
-distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)  ## Vitesse du son = 340 m/s
-
-if swishing == False and distance <= 10:
-print "swish !!"
-swishing = True
-score = score + 2
-print "score: ",score," points"
-GPIO.output(0, True)
-time.sleep(0.05)
-GPIO.output(1, True)
-time.sleep(0.05)
-GPIO.output(2, True)
-time.sleep(0.05)
-GPIO.output(3, True)
-time.sleep(0.05)
-GPIO.output(4, True)
-time.sleep(0.05)
-GPIO.output(5, True)
-time.sleep(0.05)
-GPIO.output(0, False)
-time.sleep(0.05)
-GPIO.output(1, False)
-time.sleep(0.05)
-GPIO.output(2, False)
-time.sleep(0.05)
-GPIO.output(3, False)
-time.sleep(0.05)
-GPIO.output(4, False)
-time.sleep(0.05)
-GPIO.output(5, False)
-
-if swishing == True and distance > 10:
-print "fin de swish"
-swishing = False
-
-if score > 10:
-break
-
-GPIO.cleanup()
-*/
