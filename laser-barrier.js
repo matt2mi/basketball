e54410a46379f714a7c590b8c6497277fc83dab8
matt2mi <2m1tema@gmail.com>
2019-02-24 21:38:00 +0100

    :basketball: test laser barrier #6 good one ! :tada:

--- laser-barrier.js
+++ laser-barrier.js
@@ -3,8 +3,6 @@ const Gpio = require('onoff').Gpio;
 
 module.exports = class LaserBarrier {
 
-    interval= null;
-
     init() {
         console.log('init');
         this.PHOTO_RESISTANCE = new Gpio(20, 'in');
@@ -15,15 +13,23 @@ module.exports = class LaserBarrier {
 
         console.log('startListening');
         let count = 0;
+        let ballPassing = false;
 
-        this.interval = setInterval(() => {
-            count += 1;
-            console.log('value', this.PHOTO_RESISTANCE);
-        }, 500);
+        while (count < 10) {
+            let val = this.PHOTO_RESISTANCE.readSync();
+            if (val && !ballPassing) {
+                ballPassing = !ballPassing;
+                console.log('swishing !!', val);
+                count += 2;
+            }
+            if (!val && ballPassing) {
+                ballPassing = !ballPassing;
+                console.log('ball not passing anymore');
+            }
+        }
     }
 
     stopListening() {
-        clearInterval(this.interval);
         this.PHOTO_RESISTANCE.unexport();
     }
 };
