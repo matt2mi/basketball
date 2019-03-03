e9bf7b0680f6b170c1255fc871dbfc24064812c3
matt2mi <2m1tema@gmail.com>
2019-03-04 00:03:21 +0100

    :basketball: font mono

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import {Client} from "nes";
+import {Client} from "nes";
 
 
 // TODO : font chiffres digitaux
@@ -13,7 +13,7 @@ class App extends Component {
 
     countdownTimer;
 
-    // cli = new Client('ws://192.168.0.10:3005');
+    cli = new Client('ws://192.168.0.10:3005');
 
     constructor(props) {
         super(props);
@@ -29,17 +29,15 @@ class App extends Component {
 
         this.handleScore = this.handleScore.bind(this);
         this.start = this.start.bind(this);
-        this.setClasses = this.setClasses.bind(this);
-        this.transformNumber = this.transformNumber.bind(this);
         this.startParty = this.startParty.bind(this);
         this.gameOver = this.gameOver.bind(this);
 
-        // this.cli
-        //     .connect()
-        //     .then(() => {
-        //         console.log('connected');
-        //         this.setState({connected: true});
-        //     });
+        this.cli
+            .connect()
+            .then(() => {
+                console.log('connected');
+                this.setState({connected: true});
+            });
     }
 
     handleScore(update, flags) {
@@ -50,60 +48,21 @@ class App extends Component {
     start() {
         this.setState({partyStarted: true});
 
-        this.setClasses();
+        // const yop = setInterval(() => {
+        //     this.setState({score: this.state.score + 2, countdown: this.state.countdown - 1});
+        //     if (this.state.countdown <= 0) {
+        //         clearInterval(yop);
+        //     }
+        // }, 1000);
 
-        const yop = setInterval(() => {
-            this.setState({score: this.state.score + 2, countdown: this.state.countdown - 1});
-            this.setClasses();
-            if (this.state.countdown <= 0) {
-                clearInterval(yop);
-            }
-        }, 1000);
-
-        // this.cli.subscribe('/gameover', this.gameOver);
-        // this.cli.subscribe('/swish', this.handleScore);
-        // this.cli
-        //     .request('start')
-        //     .then((data) => {
-        //         console.log(data);
-        //         this.startParty();
-        //     });
-    }
-
-    setClasses() {
-        const scoreTemp = this.state.score + '';
-        const timeTemp = this.state.countdown + '';
-
-        const valueScore = 'digit-number-front numbers' +
-            (scoreTemp[scoreTemp.length - 1] === '3' ||
-            scoreTemp[scoreTemp.length - 1] === '7' ||
-            scoreTemp[scoreTemp.length - 1] === '1' ||
-            scoreTemp === '22' ||
-            scoreTemp === '55' ?
-                ' fix-letter-spacing' : '');
-        const valueTime = 'digit-number-front numbers' +
-            (timeTemp[timeTemp.length - 1] === '3' ||
-            timeTemp[timeTemp.length - 1] === '7' ||
-            timeTemp[timeTemp.length - 1] === '1' ||
-            timeTemp === '22' ||
-            timeTemp === '55'  ?
-                ' fix-letter-spacing' : '');
-
-        console.log('score', valueScore);
-        console.log('time', valueTime);
-
-        this.setState({
-            scoreClass: valueScore,
-            timeClass: valueTime
-        });
-    }
-
-    transformNumber(nb) {
-        if(nb === 11 || nb === 21 || nb === 31) {
-            const str = '' + nb;
-            return [str.slice(0, str.length - 1), ' '].join('') + str[str.length-1];
-        }
-        return nb;
+        this.cli.subscribe('/gameover', this.gameOver);
+        this.cli.subscribe('/swish', this.handleScore);
+        this.cli
+            .request('start')
+            .then((data) => {
+                console.log(data);
+                this.startParty();
+            });
     }
 
     startParty() {
@@ -151,13 +110,13 @@ class App extends Component {
                         </div>
                         <div className="row row-front digitialism">
                             <div className="col-5 text-right">
-                                <div className={this.state.scoreClass}>
-                                    {this.transformNumber(this.state.score)}
+                                <div className="digit-number-front numbers">
+                                    {this.state.score}
                                 </div>
                             </div>
                             <div className="col-6 text-right">
-                                <div className={this.state.timeClass}>
-                                    {this.transformNumber(this.state.countdown)}
+                                <div className="digit-number-front numbers">
+                                    {this.state.countdown}
                                 </div>
                             </div>
                         </div>
